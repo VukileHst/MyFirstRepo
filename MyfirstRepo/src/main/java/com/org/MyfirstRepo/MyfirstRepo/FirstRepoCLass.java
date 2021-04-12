25cf9203fc6aed55983ac8bb5265172dd4cf32ea
Vukile <vukile@healthsystems.co.za>
2021-04-12 16:14:21 +0200

    First Commit 01

---
+++ MyfirstRepo/src/main/java/com/org/MyfirstRepo/MyfirstRepo/FirstRepoCLass.java
@@ -0,0 +1,10 @@
+package com.org.MyfirstRepo.MyfirstRepo;
+
+public class FirstRepoCLass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Messages");
+	}
+
+}
