a7352aa6f3c03b51c4f82ebfec6f06db616ca67b
Vukile <vukile@healthsystems.co.za>
2021-04-12 17:12:44 +0200

    commit

--- MyfirstRepo/src/main/java/com/org/MyfirstRepo/MyfirstRepo/FirstRepoCLass.java
+++ MyfirstRepo/src/main/java/com/org/MyfirstRepo/MyfirstRepo/FirstRepoCLass.java
@@ -1,10 +1,32 @@
 package com.org.MyfirstRepo.MyfirstRepo;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
 public class FirstRepoCLass {
 
 	public static void main(String[] args) {
+		WebDriver driver;
+		
 		// TODO Auto-generated method stub
 		System.out.println("Messages");
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+
+		// Create an instance of ChromeDriver to execute our tests
+		driver = new ChromeDriver();
+		
+		String baseUrl = "http:demo.guru99.com/test";
+		String expectedTitle = "Expected Title";
+		String actualTitle = "";
+		
+		actualTitle = driver.getTitle();
+		
+		if(actualTitle.contentEquals(expectedTitle)) {
+			System.out.println("");
+		}
+		else {
+			System.out.println("");
+		}
 	}
 
 }
